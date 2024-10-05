777827ec0661970747c4b3642e96f2e53370c054
Chris Sells <csells@sellsbrothers.com>
2024-10-05 07:11:07 -0700

    post flutterfire config

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'data/recipe_repository.dart';
+import 'firebase_options.dart';
 import 'pages/edit_recipe_page.dart';
 import 'pages/home_page.dart';
+
+// https://firebase.google.com/docs/flutter/setup
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await RecipeRepository.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(App());
 }
 
