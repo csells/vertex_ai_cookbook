5b278edf77c4d4fbd698328d49b50e7535869f87
Chris Sells <csells@sellsbrothers.com>
2024-10-07 18:57:47 -0700

    added a drawer to edit food preferences

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,7 +29,6 @@ class _HomePageState extends State<HomePage>
     chatModel: FirebaseVertexAI.instance.generativeModel(
       model: "gemini-1.5-flash",
       generationConfig: GenerationConfig(responseMimeType: 'application/json'),
-      // TODO: add a drawer with configure for system instructions
       systemInstruction: Content.system(
         '''
 You are a helpful assistant that generates recipes based on the ingredients and 
@@ -82,6 +81,61 @@ well as any trailing text commentary you care to provide:
               ),
             ],
           ),
+          drawer: Builder(builder: (context) {
+            final controller = TextEditingController(
+              text: Settings.foodPreferences,
+            );
+            return Drawer(
+              child: ListView(
+                children: [
+                  const DrawerHeader(child: Text('Food Preferences')),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: controller,
+                      maxLines: 5,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your food preferences...',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OverflowBar(
+                        spacing: 8,
+                        children: [
+                          ElevatedButton(
+                            child: const Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          OutlinedButton(
+                            child: const Text('Save'),
+                            onPressed: () {
+                              Settings.setFoodPreferences(controller.text);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
           body: snapshot.hasData
               ? _RowOrTabBar(
                   tabs: const [
