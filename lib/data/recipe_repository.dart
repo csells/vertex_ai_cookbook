885637682f75cab7a4426ae5fc70feabc85c2dc0
Chris Sells <csells@sellsbrothers.com>
2024-10-10 21:23:51 -0700

    wip

--- lib/data/recipe_repository.dart
+++ lib/data/recipe_repository.dart
@@ -2,86 +2,79 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 import 'recipe_data.dart';
 
+// TODO: only access recipes from the currently logged in user
 class RecipeRepository {
+  RecipeRepository._(this._recipesCollection);
+  final CollectionReference _recipesCollection;
+
+  static Future<RecipeRepository> getUserRepository(User user) async {
+    final recipesCollection =
+        FirebaseFirestore.instance.collection('users/${user.uid}/recipes');
+    final repo = RecipeRepository._(recipesCollection);
+    await repo._loadRecipes();
+    return repo;
+  }
+
   static const newRecipeID = '__NEW_RECIPE__';
-  static const _assetFileName = 'assets/recipes_default.json';
+  static const _defaultRecipesAsset = 'assets/recipes_default.json';
 
-  static List<Recipe>? _recipes;
-  static final items = ValueNotifier<Iterable<Recipe>>([]);
+  late List<Recipe> _recipes;
+  final items = ValueNotifier<Iterable<Recipe>>([]);
+  Iterable<Recipe> get recipes => items.value;
 
-  // TODO: only access recipes from the currently logged in user
-  static get _recipesCollection =>
-      FirebaseFirestore.instance.collection('recipes');
+  Recipe getRecipe(String recipeId) => (recipeId == newRecipeID)
+      ? Recipe.empty(newRecipeID)
+      : _recipes.singleWhere((r) => r.id == recipeId);
 
-  static Future<void> init() async {
-    assert(_recipes == null, 'call RecipeRepository.init() exactly once');
-    await _loadRecipes();
-  }
-
-  static Iterable<Recipe> get recipes {
-    assert(_recipes != null, 'call RecipeRepository.init() exactly once');
-    return _recipes!;
-  }
-
-  static Recipe getRecipe(String recipeId) {
-    assert(_recipes != null, 'call RecipeRepository.init() exactly once');
-    if (recipeId == newRecipeID) return Recipe.empty(newRecipeID);
-    return _recipes!.singleWhere((r) => r.id == recipeId);
-  }
-
-  static Future<void> addNewRecipe(Recipe newRecipe) async {
-    assert(_recipes != null, 'call RecipeRepository.init() exactly once');
-    _recipes!.add(newRecipe);
+  Future<void> addNewRecipe(Recipe newRecipe) async {
+    _recipes.add(newRecipe);
     await _recipesCollection.doc(newRecipe.id).set(newRecipe.toJson());
     _notifyListeners();
   }
 
-  static Future<void> updateRecipe(Recipe recipe) async {
-    assert(_recipes != null, 'call RecipeRepository.init() exactly once');
-    final i = _recipes!.indexWhere((r) => r.id == recipe.id);
+  Future<void> updateRecipe(Recipe recipe) async {
+    final i = _recipes.indexWhere((r) => r.id == recipe.id);
     assert(i >= 0);
-    _recipes![i] = recipe;
+    _recipes[i] = recipe;
     await _recipesCollection.doc(recipe.id).update(recipe.toJson());
     _notifyListeners();
   }
 
-  static Future<void> deleteRecipe(Recipe recipe) async {
-    assert(_recipes != null, 'call RecipeRepository.init() exactly once');
-    final removed = _recipes!.remove(recipe);
+  Future<void> deleteRecipe(Recipe recipe) async {
+    final removed = _recipes.remove(recipe);
     assert(removed);
     await _recipesCollection.doc(recipe.id).delete();
     _notifyListeners();
   }
 
-  static Future<void> _loadRecipes() async {
-    final recipesCollection = _recipesCollection;
-
+  Future<void> _loadRecipes() async {
     // Check if the collection exists and has documents
-    final snapshot = await recipesCollection.limit(1).get();
+    final snapshot = await _recipesCollection.limit(1).get();
     if (snapshot.docs.isEmpty) {
       // If the collection is empty, seed it with default recipes
-      final contents = await rootBundle.loadString(_assetFileName);
+      final contents = await rootBundle.loadString(_defaultRecipesAsset);
       final jsonList = json.decode(contents) as List;
       final defaultRecipes =
           jsonList.map((json) => Recipe.fromJson(json)).toList();
 
       // Add default recipes to Firestore
       for (var recipe in defaultRecipes) {
-        await recipesCollection.doc(recipe.id).set(recipe.toJson());
+        await _recipesCollection.doc(recipe.id).set(recipe.toJson());
       }
 
       _recipes = defaultRecipes;
     } else {
       // If the collection exists and has documents, fetch all recipes
-      final querySnapshot = await recipesCollection.get();
+      final querySnapshot = await _recipesCollection.get();
       final recipes = <Recipe>[];
       for (var doc in querySnapshot.docs) {
-        recipes.add(Recipe.fromJson(doc.data()));
+        recipes.add(Recipe.fromJson(doc.data() as Map<String, dynamic>));
       }
 
       _recipes = recipes;
@@ -90,9 +83,8 @@ class RecipeRepository {
     _notifyListeners();
   }
 
-  static void _notifyListeners() {
-    assert(_recipes != null);
+  void _notifyListeners() {
     items.value = [];
-    items.value = _recipes!;
+    items.value = _recipes;
   }
 }
